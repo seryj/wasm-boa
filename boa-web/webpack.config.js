38685f2e3ff7c778634469e37ac0aff6acc66976
seryj <lapuh@gmx.de>
2020-12-25 23:04:21 +0100

    Not sure if these files are needed but keep them

--- boa-web/webpack.config.js
+++ boa-web/webpack.config.js
@@ -1,14 +1,45 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
+const glob = require('glob');
 
 module.exports = {
-  entry: "./bootstrap.js",
-  output: {
+  entry: {
+    bootstrap: path.resolve(__dirname, "./bootstrap.js"),
+    style: path.resolve(__dirname, './static/style.css'),
+    images: glob.sync(path.resolve(__dirname, './static/images/*.*')),
+  },
+  /*output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
+  },*/
+  output: {
+    path: path.resolve('./dist/'),
+    publicPath: '/static/',
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,        
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /(\.woff2?|\.woff|\.ttf|\.eot|\.svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=[name]-[hash:6].[ext]',
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/,
+        loader: 'file-loader?name=[name].[ext]',
+      },
+    ],
   },
   mode: "development",
   plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, ".", "./static/index.html")
+    })
+  ]
+  /*plugins: [
     new CopyWebpackPlugin(['index.html'])
-  ],
+  ],*/
 };
